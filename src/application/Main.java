31d045d6eaf6222e3eaf654bc014cc31a3f14f42
marce <marce@Marcelo-Magistral>
2021-09-17 10:00:00 -0300

    Revert "About view"
    This reverts commit 970d8fe21206acd257332c6e999d39b77e244ab2.

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,9 +9,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	private static Scene mainScene;
-	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -21,7 +18,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			mainScene = new Scene(scrollPane);
+			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -29,13 +26,4 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
-	public static Scene getMainScene() {
-		return mainScene;
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
 }
